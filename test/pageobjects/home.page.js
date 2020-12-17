d4675d53f7ae26bb7194c8ae9980a661016e1dc9
Kevin Hernandez <khkevinhernandez@gmail.com>
2020-12-16 22:56:03 -0600

    Second test - input search get focus and update placeholder

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -5,6 +5,7 @@ class HomePage extends Page {
     get inputSearch () { return $('#search-input') }
     get btnSearch () { return $('.btn_search') }
     get lblInfo () { return $('.col-lg-6 h3')}
+    get lblOcupacional () {return $('label=Ocupacional')}
 
     doSearch(term){
         this.inputSearch.setValue(term);
--- test/specs/home.e2e.js
+++ test/specs/home.e2e.js
@@ -1,14 +1,29 @@
 const HomePage = require('../pageobjects/home.page');
 
-describe('My Home application', () => {
+describe('The Home Search Functionality', () => {
+
+    before(()=> {
+        HomePage.open();
+    })
+
     it('should remain on home page after search with blank criteria', () => {
 
         const infoText = '¿Buscas un Terapeuta?';
 
-        HomePage.open();
+
         const url = browser.getUrl();
         HomePage.doSearch('');
         expect(HomePage.lblInfo).toHaveText(infoText, {ignoreCase: true});
         expect(url).toEqual('https://develop.terapeutica.digital/#/');
     });
+
+    it('should update the placeholder and search input get the focus', () => {
+
+        HomePage.lblOcupacional.click();
+        const placeHolderValue = '¿Buscas a alguien o algo en específico?';
+
+        expect(HomePage.inputSearch).toBeFocused();
+        expect(HomePage.inputSearch).toHaveAttr('placeholder', placeHolderValue);
+    });
+
 });
